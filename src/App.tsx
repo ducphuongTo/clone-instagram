615c61552c7c81245c27447830999602f0c6db2c
phuong.toduc <phuong.toduc@nastechglobal.com>
2024-03-24 21:54:38 +0700

    Init Sign Up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import './global.css'
-
+import { Route, Routes } from 'react-router'
+import SignInForm from './_auth/forms/SignInForm'
+import { Home } from './_root/pages'
+import SignUpForm from './_auth/forms/SignUpForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
 
 const App = () => {
   return (
-    <h1 className='text-3xl font-bold underline'>
-        Hello World
-    </h1>
+    <main className='flex h-screen'>
+      <Routes>
+        {/*public routes*/}
+        <Route element={<AuthLayout/>}>
+          <Route path='/sign-in'  element={<SignInForm/>}/>
+          <Route path='/sign-up'  element={<SignUpForm/>}/>
+        </Route>
+          
+
+
+        {/*private routes*/}
+          <Route element={<RootLayout/>}>
+            <Route index element={<Home/>}/>
+          </Route>
+          
+      </Routes>
+    </main>
   )
 }
 
