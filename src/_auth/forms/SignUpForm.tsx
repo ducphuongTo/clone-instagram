edf8b3e1ad1428bedfa942aaac0e6ce2fd5d786f
phuong.toduc <phuong.toduc@nastechglobal.com>
2024-03-25 20:58:47 +0700

    Create Sign up page

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -5,39 +5,61 @@ import { useForm } from "react-hook-form"
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
+import { signUpValidation } from '@/lib/validation'
+import Loader from '@/components/shared/Loader'
+import { Link } from 'react-router-dom'
 
 const SignUpForm = () => {
-  
+  const isLoading = false
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof signUpValidation>>({
+    resolver: zodResolver(signUpValidation),
     defaultValues: {
+      name: '',
       username: "",
+      email: "",
+      password: ""
     },
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+  async function onSubmit(values: z.infer<typeof signUpValidation>) {
+    // const newUsers = await createUserAccount(values)
     console.log(values)
   }
 
   return (
-    <div>
       <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className='sm:w-420 flex-center flex-col'>
+          <img src="/assets/images/logo.svg" alt="logo" />
+          <h2 className='h3-bold md:h2-bold pt-5 sm:pt-12'>Create a new account</h2>
+          <p className='text-light-3 small-medium md:base-regular'>To use Snapgram enter your account details</p>
+        
+          <form 
+            onSubmit={form.handleSubmit(onSubmit)} 
+            className="flex flex-col gap-5 w-full mt-4"
+            name='name'
+          >
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input type='text' className='shad-input' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
             <FormField
               control={form.control}
               name="username"
@@ -45,19 +67,60 @@ const SignUpForm = () => {
                 <FormItem>
                   <FormLabel>Username</FormLabel>
                   <FormControl>
-                    <Input placeholder="shadcn" {...field} />
+                    <Input type='text' className='shad-input' {...field} />
                   </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
             />
-            <Button type="submit">Submit</Button>
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type='email' className='shad-input' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type='password' className='shad-input' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            
+            <Button type="submit" className='shad-button_primary'>
+              {
+                isLoading ? (
+                  <div className='flex-center gap02'>
+                    <Loader/>
+                  </div>
+                ) : "Sign up"
+              }
+            </Button>
+            <p className='text-small-regular text-lighjt-2 text-center mt-2'>
+              Already have an account ?
+              <Link to="/sign-in" className='text-primary-500 text-small-semibold ml-1'>
+                Log in
+              </Link>
+            </p>
           </form>
+        </div>
       </Form>
-    </div>
   )
 }
 
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,9 @@
+import { z } from "zod"
+
+
+export const signUpValidation = z.object({
+    name: z.string().min(2, { message: "Name is too short"}),
+    username: z.string().min(2, { message: "UserName is too short"}).max(50),
+    email: z.string().email(),
+    password: z.string().min(8, {message: "Password must be at least 8 character"})
+  })
